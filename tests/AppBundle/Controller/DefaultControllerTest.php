a2226ea265baedba7d4b8c43a76da444fce3633d
Julien_Plumecocq <b4ckfr0mh3ll@gmail.com>
2020-10-26 15:54:46 +0100

    change phpunit version to work with symfony 3.1

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\AppBundle\Controller;
 
+use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+
     public function testIndex()
     {
+        echo "toto";
+
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
@@ -15,4 +19,5 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
 }
