1167b1dcb2203f03b8738e6a213091fe26ecbfab
Julien_Plumecocq <b4ckfr0mh3ll@gmail.com>
2020-11-02 12:02:17 +0100

    add phpunit tests for controllers routes

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -1,22 +1,23 @@
 <?php
 
+
 namespace Tests\AppBundle\Controller;
+
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class TaskControllerTest extends WebTestCase
 {
-
-    public function testIndex()
+    public function testList()
     {
-        $client = static::createClient(array(), array(
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/tasks', array(), array(), array(
             'PHP_AUTH_USER' => 'julien',
             'PHP_AUTH_PW' => 'password'
         ));
 
-        $crawler = $client->request('GET', '/');
-
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('div.caption')->count());
     }
-
 }
---
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Tests\AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    public function testList()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/users', array(), array(), array(
+            'PHP_AUTH_USER' => 'julien',
+            'PHP_AUTH_PW' => 'password'
+        ));
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCreate()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/users/create', array(), array(), array(
+            'PHP_AUTH_USER' => 'julien',
+            'PHP_AUTH_PW' => 'password'
+        ),[
+            'username' => 'test',
+            'password' => 'password'
+        ]);
+
+
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function testEdit()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/users/2/edit', array(), array(), array(
+            'PHP_AUTH_USER' => 'julien',
+            'PHP_AUTH_PW' => 'password'
+        ));
+
+
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+}
