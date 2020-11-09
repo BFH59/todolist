f6f4e2a9efd551d62ef4639e2d9e1d7e9ff1a056
Julien_Plumecocq <b4ckfr0mh3ll@gmail.com>
2020-11-09 15:34:37 +0100

    fix phpunit and create security.yaml for test env

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -9,14 +9,11 @@ class DefaultControllerTest extends WebTestCase
 
     public function testIndex()
     {
-        $client = static::createClient(array(), array(
-            'PHP_AUTH_USER' => 'julien',
-            'PHP_AUTH_PW' => 'password'
-        ));
+        $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
 
 }
